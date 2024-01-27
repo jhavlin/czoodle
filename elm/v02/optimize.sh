b4c06892a46c9539a0b3e2d97d64508157dd70a4
Jaroslav Havlin <380698+jhavlin@users.noreply.github.com>
2024-01-27 17:24:47 +0100

    Update scripts

--- elm/v02/optimize.sh
+++ elm/v02/optimize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-elm make --optimize src/CreatePage.elm --output ../../static/v01/js/create.elm.js
-elm make --optimize src/VotePage.elm --output ../../static/v01/js/vote.elm.js
+elm make --optimize src/CreatePage.elm --output ../../static/v02/js/create.elm.js
+elm make --optimize src/VotePage.elm --output ../../static/v02/js/vote.elm.js
---
+++ elm/v02/start-live.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+elm-live src/CreatePage.elm --start-page=index.html --dir=../../static --hot -- --output=../../static/v02/js/create.elm.js
