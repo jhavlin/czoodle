0ee9ea48f2abec4937ea965f76628d6f2ac20690
Jaroslav Havlin <380698+jhavlin@users.noreply.github.com>
2025-05-01 12:17:36 +0200

    Restart project almost from scratch

--- elm/v01/watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-inotifywait -m -r ./src --format '%w%f' -e modify |
-    while read file; do
-        echo "> Changes detected >>>>"
-        echo $file
-        #   elm-format --yes src/
-        elm make src/CreatePage.elm --output ../../static/v01/js/create.elm.js
-        elm make src/VotePage.elm --output ../../static/v01/js/vote.elm.js
-    done
--- elm/v02/start-live.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-elm-live src/CreatePage.elm --start-page=index.html --dir=../../static --hot -- --output=../../static/v02/js/create.elm.js
