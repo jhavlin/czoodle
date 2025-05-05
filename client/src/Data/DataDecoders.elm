a22d290ebe559c83562593a2606ee1a6e2010ec3
Jaroslav Havlin <380698+jhavlin@users.noreply.github.com>
2025-05-05 23:42:02 +0200

    Update decoders for new data model

--- client/src/Data/DataDecoders.elm
+++ client/src/Data/DataDecoders.elm
@@ -1,4 +1,4 @@
-module Data.DataDecoders exposing (decodePollInfo)
+module Data.DataDecoders exposing (decodeProject)
 
 import Candidate.DateCandidate.DateCandidateEncoding exposing (decodeDateCandidateItem)
 import Candidate.TextCandidate.TextCandidateEncoding exposing (decodeTextCandidateItem)
@@ -6,12 +6,16 @@ import Common.CommonUtils exposing (stringToMaybe)
 import Data.DataModel
     exposing
         ( CandidatesInfo(..)
+        , Poll
         , PollId(..)
         , PollInfo
-        , VoterId(..)
+        , Project
+        , Voter
+        , VotesInfo(..)
         )
+import Data.VoterId exposing (VoterId(..))
 import Json.Decode as D
-import Data.DataModel exposing (VotesInfo)
+import Poll.YesNoPoll.YesNoPollEncoding exposing (decodeYesNoVotesInfo)
 
 
 decodeCandidatesInfo : D.Decoder CandidatesInfo
@@ -42,14 +46,10 @@ decodeCandidatesInfo =
 decodeVotesInfo : D.Decoder VotesInfo
 decodeVotesInfo =
     let
-        yesNoVotesInfoDecoder =
-            D.map DateCandidatesInfo <|
-                D.field "items" (D.list decodeDateCandidateItem)
-
         choose type_ =
             case type_ of
                 "yesNo" ->
-                    dateCandidatesInfoDecoder
+                    D.map YesNotVotesInfo decodeYesNoVotesInfo
 
                 _ ->
                     D.fail <| "Invalid 'candidates' type " ++ type_
@@ -57,101 +57,38 @@ decodeVotesInfo =
     D.andThen choose <| D.field "type" D.string
 
 
-
-{-
-
-
-
-   decodePollInfo : D.Decoder PollInfo
-   decodePollInfo =
-       let
-           datePollInfoDecoder =
-               D.map (\l -> DatePollInfo { items = l }) <|
-                   D.field "items" (D.list decodeDateCandidateItem)
-
-           genericPollInfoDecoder =
-               D.map (\l -> GenericPollInfo { items = l }) <|
-                   D.field "items" (D.list decodeTextCandidateItem)
-
-           choose type_ =
-               case type_ of
-                   "date" ->
-                       datePollInfoDecoder
-
-                   "generic" ->
-                       genericPollInfoDecoder
-
-                   _ ->
-                       genericPollInfoDecoder
-       in
-       D.andThen choose <| D.field "type" D.string
+decodePollInfo : D.Decoder PollInfo
+decodePollInfo =
+    D.map2 PollInfo
+        (D.field "candidates" decodeCandidatesInfo)
+        (D.field "votes" decodeVotesInfo)
 
 
-
-   decodePersonRow : D.Decoder PersonRow
-   decodePersonRow =
-       let
-           pairsDecoder : D.Decoder (List ( String, SelectedOption ))
-           pairsDecoder =
-               D.keyValuePairs decodeSelectedOption
-
-           foldFn : ( String, SelectedOption ) -> Maybe (Dict Int SelectedOption) -> Maybe (Dict Int SelectedOption)
-           foldFn ( strKey, value ) acc =
-               Maybe.map2 (\int dict -> Dict.insert int value dict) (String.toInt strKey) acc
-
-           pairsToDict : List ( String, SelectedOption ) -> D.Decoder (Dict Int SelectedOption)
-           pairsToDict pairs =
-               let
-                   dictionary : Maybe (Dict Int SelectedOption)
-                   dictionary =
-                       List.foldl foldFn (Just Dict.empty) pairs
-               in
-               case dictionary of
-                   Just d ->
-                       D.succeed d
-
-                   Nothing ->
-                       D.fail "all option keys have to be convertible to integers"
-
-           votesDictDecoder : D.Decoder (Dict Int SelectedOption)
-           votesDictDecoder =
-               D.andThen pairsToDict pairsDecoder
-       in
-       D.map3 PersonRow
-           (D.map PersonId <| D.field "id" D.int)
-           (D.field "name" D.string)
-           (D.field "options" votesDictDecoder)
+decodePoll : D.Decoder Poll
+decodePoll =
+    D.map4 Poll
+        (D.map PollId <| D.field "id" D.int)
+        (D.map stringToMaybe <| D.field "title" D.string)
+        (D.maybe <| D.field "description" D.string)
+        (D.field "def" decodePollInfo)
 
 
-   decodePoll : D.Decoder Poll
-   decodePoll =
-       D.map6 Poll
-           (D.map PollId <| D.field "id" D.int)
-           (D.map stringToMaybe <| D.field "title" D.string)
-           (D.maybe <| D.field "description" D.string)
-           (D.field "def" decodePollInfo)
-           (D.field "people" <| D.list decodePersonRow)
-           (D.field "lastPersonId" D.int)
+decodeVoter : D.Decoder Voter
+decodeVoter =
+    D.map2 Voter
+        (D.map VoterId <| D.field "voterId" D.int)
+        (D.field "name" D.string)
 
 
-   decodeComment : D.Decoder Comment
-   decodeComment =
-       D.map2 Comment
-           (D.map CommentId <| D.field "id" D.int)
-           (D.field "text" D.string)
-
-
-   decodeProject : D.Value -> Result D.Error Project
-   decodeProject json =
-       let
-           projectDecoder =
-               D.map5 Project
-                   (D.map stringToMaybe (D.field "title" <| D.string))
-                   (D.field "polls" <| D.list decodePoll)
-                   (D.field "lastPollId" D.int)
-                   (D.field "comments" <| D.list decodeComment)
-                   (D.field "lastCommentId" D.int)
-       in
-       D.decodeValue projectDecoder json
-
--}
+decodeProject : D.Value -> Result D.Error Project
+decodeProject json =
+    let
+        projectDecoder =
+            D.map5 Project
+                (D.map stringToMaybe (D.field "title" <| D.string))
+                (D.field "polls" <| D.list decodePoll)
+                (D.field "lastPollId" D.int)
+                (D.field "voters" <| D.list decodeVoter)
+                (D.field "lastVoterId" D.int)
+    in
+    D.decodeValue projectDecoder json
